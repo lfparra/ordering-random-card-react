84f5da159f317ba27134f4caddbb999fbac74e94
spizarrozavala <spizarrozavala@gmail.com>
2020-06-05 20:10:01 +0000

    actualizacion sebastian

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Formulario } from './formulario';
+import Card from "./card";
 
 const App = () => {
 
-    const[state, setState]=useState({
+    const [cartas, setCartas] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+    const [pintas, setPintas] = useState(['spades', 'club', 'heart', 'diams']);
+    const [randomCartas, setRandomCartas] = useState(null);
+    const [randomPintas, setRandomPintas] = useState(null);
+    const [todasLasCartas, setTodasLasCartas] = useState([]);
+
+    const [state, setState] = useState({
         valorInput: ""
     })
 
-    const consigueValor =(e)=>{
+    function randomValues(arr) {
+        return (
+            Math.floor(Math.random() * arr.length)
+        )
+    }
+
+
+
+    const consigueValor = (e) => {
         console.log(e.target.value);
 
         setState({
@@ -16,10 +31,12 @@ const App = () => {
         })
     }
 
-    const draw = (e)=>{
-        console.log(e.target.id);
+    const draw = (e) => {
+        for (let i = 0; i < state.valorInput; i++){
+            
+        }
     }
-    const sort = (e)=>{
+    const sort = (e) => {
         console.log(e.target.id);
     }
 
@@ -28,7 +45,7 @@ const App = () => {
         <>
             <div className="Contenedor">
                 <div className="header">
-                    <Formulario consigueValor={consigueValor} draw={draw} sort={sort}   />
+                    <Formulario consigueValor={consigueValor} draw={draw} sort={sort} />
                     <div className="card"></div>
                 </div>
                 <div className="content">
---
+++ src/components/card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card(){
+    return(
+        <>
+        <div class="carta"> 
+        <div class="numero spades"> A </div> 
+        </div>
+        </>
+    )
+
+
+
+}
+
+export default Card;
