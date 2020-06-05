3a7ca30f2f0416d2d22a409038ffe511954986da
lfparra <leoparra95@gmail.com>
2020-06-05 19:09:42 +0000

    Inicio proyecto en conjunto

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+        <div className="header">
+            <h2></h2>
+            <input></input>
+            <button></button>
+            <button></button>
+            <div className="card"></div>
+        </div>
+        <div className="content">
+            <h2></h2>
+            <div className="card"></div>
+        </div>
+        </>
+    )
+}
+
+
+export default App;
